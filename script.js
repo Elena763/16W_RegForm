651cc38114db3fc6ffa4ca5da9143f2a34ad4859
Elena Vasileva <123897353+Elena763@users.noreply.github.com>
2023-07-02 23:57:25 +0300

    Added files
    Added files

---
+++ script.js
@@ -0,0 +1,16 @@
+let regForm = document.forms;
+let button = document.getElementById("button");
+
+const username = document.getElementById("name");
+const mail = document.getElementById("email");
+const age = document.getElementById("age");
+const sex = document.querySelectorAll('input[name="sex"]');
+const profession = document.getElementById("profession");
+const password = document.getElementById("password");
+const rePassword = document.getElementById("repassword");
+const agree = document.forms.regFormm.elements.checkbox;
+
+
+form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+});
