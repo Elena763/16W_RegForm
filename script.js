a0d896629f27445f4c68474cd779dcc53bbe9fea
Elena763 <elena.evg@bk.ru>
2023-07-03 22:10:43 +0300

    Correct files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let regForm = document.forms;
+/*let regForm = document.forms;
 let button = document.getElementById("button");
 
 const username = document.getElementById("name");
@@ -8,9 +8,31 @@ const sex = document.querySelectorAll('input[name="sex"]');
 const profession = document.getElementById("profession");
 const password = document.getElementById("password");
 const rePassword = document.getElementById("repassword");
-const agree = document.forms.regFormm.elements.checkbox;
+const agree = document.forms.regFormm.elements.checkbox;*/
 
+let errors = [];
+function checkValidity(input) {
+    let validity = input.validity;
+    console.log(validity);
+    if (validity.valueMissing) { errors.push('Поле ' + input.placeholder + ' не заполнено'); }
+    if (validity.patternMismatch) { errors.push('Неверный формат заполнения'); }
+    if (validity.rangeOverflow) { let max = getAttributeValue(input, 'max');
+    errors.push('Максимальное значение поля не может быть больше чем ' + max);}
+    //console.log(max);
+    if (validity.rangeUnderflow) { let min = getAttributeValue(input, 'min');
+    errors.push('Минимальное значение поля не может быть меньше чем ' + min);}
+    //console.log(min);
+};
 
-form.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-});
+function checkAll() {
+    form.addEventListener('submit', function (evt) {
+        evt.preventDefault();});
+    errors = [];
+    let inputs = document.querySelectorAll("input");
+    for (let input of inputs) {
+        checkValidity(input);
+    }
+
+    console.log(errors);
+    document.getElementById('error').innerHTML = errors.join(', <br>');
+};
